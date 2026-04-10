2fee49aec3798061a30c1fe2f8e9414229b94fef
Soumen Mishra <soumenmishra187@gmail.com>
2026-04-10 20:49:37 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const TOKEN = "11a9dd37a145dfe58f0f8eb17a4c5df3e9e4f86b"; // Replace with your actual API token from waqi.info
+const TOKEN = "11a9dd37a145dfe5###############"; // Replace with your actual API token from waqi.info
 
 export const getCityAQI = async (city) => {
   try {
